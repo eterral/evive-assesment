3b93cec209e1bc721ef49366ee847edf60f84306
eterral <elliott@eterral.com>
2022-02-22 09:21:03 -0600

    finishing up testing

---
+++ __tests__/test.js
@@ -0,0 +1,86 @@
+let order = require("../diner.js");
+
+//General tests
+test("test if meal has ids", () => {
+  expect(order.processOrder("breakfast")).toBe("Invalid input");
+});
+
+test("test if meal type is vaild", () => {
+  expect(order.processOrder("Lupper 1,2,3")).toBe("Invalid meal type");
+});
+
+//Breakfast specific tests
+
+test("Test if multiple mains are ordered for breakfast", () => {
+  expect(order.processOrder("Breakfast 1,1")).toBe(
+    "Unable to process. Eggs can not be ordered more than once"
+  );
+});
+
+test("Test multiple coffees for breakfast", () => {
+  multipleCoffees = {
+    1: 1,
+    2: 1,
+    3: 2,
+  };
+  expect(order.breakfastOrder(multipleCoffees)).toBe("Eggs Toast Coffee(2)");
+});
+
+test("Test water for breakfast if no drink is ordered", () => {
+  breakfast = {
+    1: 1,
+    2: 1,
+  };
+  expect(order.breakfastOrder(breakfast)).toBe("Eggs Toast Water");
+});
+
+//Lunch specific tests
+test("Test if multiple mains are ordered for lunch", () => {
+  expect(order.processOrder("Lunch 1,1")).toBe(
+    "Unable to process. Sandwich can not be ordered more than once"
+  );
+});
+
+test("Test multiple sides for lunch", () => {
+  multipleSides = {
+    1: 1,
+    2: 2,
+    3: 1,
+  };
+  expect(order.lunchOrder(multipleSides)).toBe("Sandwich Chips(2) Soda");
+});
+
+test("Test water for lunch if no drink is ordered", () => {
+  lunch = {
+    1: 1,
+    2: 1,
+  };
+  expect(order.lunchOrder(lunch)).toBe("Sandwich Chips Water");
+});
+
+//Dinner specific tests
+test("Test if multiple mains are ordered for dinner", () => {
+  expect(order.processOrder("Dinner 1,1")).toBe(
+    "Unable to process. Steak can not be ordered more than once"
+  );
+});
+
+test("Test water for dinner if no drink is ordered", () => {
+  dinner = {
+    1: 1,
+    2: 1,
+    4: 1,
+  };
+  expect(order.dinnerOrder(dinner)).toBe("Steak Potatoes Water Cake");
+});
+
+test("Test dessert was ordered for dinner", () => {
+  dinner = {
+    1: 1,
+    2: 1,
+    3: 1,
+  };
+  expect(order.dinnerOrder(dinner)).toBe(
+    "Unable to process. Dessert is missing"
+  );
+});
--- diner.js
+++ diner.js
@@ -1,78 +1,151 @@
-import readlineSync from "readline-sync";
+// import readlineSync from "readline-sync";
 
-// function getInput(prompt) {
-//   return readlineSync.question(`${prompt}: `);
-// }
-
-// function greet() {
-//   let meal = getInput(
-//     "Welcome to the diner, Do you want Breakfast, Lunch, or Dinner?"
-//   );
-//   if (meal.toLowerCase() === "breakfast") {
-//     breakfast();
-//   } else if (meal.toLowerCase() === "lunch") {
-//     lunch();
-//   } else if (meal.toLocaleLowerCase === "dinner") {
-//     dinner();
-//   } else {
-//     console.log("Please enter either Breakfast, Lunch, or Dinner");
-//     greet();
-//   }
-// }
-
-// greet();
-
-function order() {
-  let order = readlineSync.question(`Enter you order: `);
-  let reciept = "";
+function processOrder(order) {
+  // let order = readlineSync.question(`Enter you order: `);
+  // declare items as object to total mains/sides/drinks/desserts
   let items = {};
-  order = order.split(" ");
-  const meal = order[0];
-  order = order[1].split(",");
-  order.forEach((item) => {
+  // split meal from items ordered
+  processedOrder = order.split(" ");
+  if (processedOrder.length != 2) {
+    return "Invalid input";
+  }
+  //declare meal type
+  const meal = processedOrder[0].toLowerCase();
+  //seperate items and count them in items object
+  processedOrder = processedOrder[1].split(",");
+  processedOrder.forEach((item) => {
     items[item] = (items[item] || 0) + 1;
   });
-  console.log(items);
-  if (!order) {
+  if (!items["1"] && !items["2"]) {
     return "Unable to process. Main is missing. Side is missing";
   }
-  // if (meal.toLocaleLowerCase() === "breakfast") {
-  //   if (order[0] === "1" && meal.toLocaleLowerCase() === "breakfast") {
-  //     reciept = reciept + "Eggs ";
-  //     order.shift();
-  //     if (order[0] === "1") {
-  //       return console.log("Unable to process. You can only order one main");
-  //     }
-  //   } else if (order[0] === "1" && meal.toLocaleLowerCase() === "lunch") {
-  //     reciept = reciept + "Sandwhich ";
-  //     order.shift();
-  //     if (order[0] === "1") {
-  //       return console.log("Unable to process. You can only order one main");
-  //     }
-  //   } else if (order[0] === "1" && meal.toLocaleLowerCase() === "dinner") {
-  //     reciept = reciept + "Steak ";
-  //   }
-  //   else {
-  //     return console.log("Unable to process. You must order a main.");
-  //   }
-  //   order.shift();
-  //   if (order[0] === "1") {
-  //     return console.log("Unable to process. You can only order one main");
-  //   }
-  //   if (order[0] === "2") {
-  //     reciept = reciept + "Toast ";
-  //     order.shift();
-  //   } else {
-  //     return console.log("Unable to process. You must order a side");
-  //   }
-  //   let drinks = 0;
-  //   order.forEach((drink) => {
-  //     drinks++;
-  //   });
-  //   reciept = reciept + `Coffee(${drinks})`;
-  // }
-  // console.log(reciept);
-  // return reciept;
+  //Check if there is more than one main ordered
+  if (items["1"] > 1) {
+    switch (meal) {
+      case "breakfast":
+        return "Unable to process. Eggs can not be ordered more than once";
+      case "lunch":
+        return "Unable to process. Sandwich can not be ordered more than once";
+      case "dinner":
+        return "Unable to process. Steak can not be ordered more than once";
+    }
+  }
+  //Check if more than 1 side is ordered for breakfast or dinner
+  if (items["2"] > 1 && meal != "lunch") {
+    return "Unable to process. Can only order one side";
+  }
+  //Check which meal function to call
+  switch (meal) {
+    case "breakfast":
+      return breakfastOrder(items);
+    case "lunch":
+      return lunchOrder(items);
+    case "dinner":
+      return dinnerOrder(items);
+    default:
+      return "Invalid meal type";
+  }
 }
 
-order();
+function breakfastOrder(items) {
+  let reciept = "";
+  let breakfastMenuItems = {
+    1: "Eggs",
+    2: "Toast",
+    3: "Coffee",
+  };
+  //Check if both main and side is missing
+  //Create reciept output for breakfast
+  if (items["1"] > 1) {
+    reciept += `${breakfastMenuItems["1"]}(${items["1"]}) `;
+  } else {
+    reciept += `${breakfastMenuItems["1"]} `;
+  }
+  if (items["2"] > 1) {
+    reciept += `${breakfastMenuItems["2"]}(${items["2"]}) `;
+  } else {
+    reciept += `${breakfastMenuItems["2"]} `;
+  }
+  if (items["3"] > 1) {
+    reciept += `${breakfastMenuItems["3"]}(${items["3"]})`;
+  } else if (items["3"] === 1) {
+    reciept += `${breakfastMenuItems["3"]}`;
+  } else {
+    reciept += "Water";
+  }
+  console.log(reciept);
+  return reciept;
+}
+
+function lunchOrder(items) {
+  let reciept = "";
+  let lunchMenuItems = {
+    1: "Sandwich",
+    2: "Chips",
+    3: "Soda",
+  };
+  //Check if both main and side is missing
+  //Create reciept output for lunch
+  if (items["1"] > 1) {
+    reciept += `${lunchMenuItems["1"]}(${items["1"]}) `;
+  } else {
+    reciept += `${lunchMenuItems["1"]} `;
+  }
+  if (items["2"] > 1) {
+    reciept += `${lunchMenuItems["2"]}(${items["2"]}) `;
+  } else {
+    reciept += `${lunchMenuItems["2"]} `;
+  }
+  if (items["3"] > 1) {
+    reciept += `${lunchMenuItems["3"]}(${items["3"]})`;
+  } else if (items["3"] === 1) {
+    reciept += `${lunchMenuItems["3"]}`;
+  } else {
+    reciept += "Water";
+  }
+  console.log(reciept);
+  return reciept;
+}
+function dinnerOrder(items) {
+  let reciept = "";
+  let dinnerMenuItems = {
+    1: "Steak",
+    2: "Potatoes",
+    3: "Wine",
+    4: "Cake",
+  };
+  //Create reciept output for dinner
+  if (items["1"] > 1) {
+    reciept += `${dinnerMenuItems["1"]}(${items["1"]}) `;
+  } else {
+    reciept += `${dinnerMenuItems["1"]} `;
+  }
+  if (items["2"] > 1) {
+    reciept += `${dinnerMenuItems["2"]}(${items["2"]}) `;
+  } else {
+    reciept += `${dinnerMenuItems["2"]} `;
+  }
+  if (items["3"] > 1) {
+    reciept += `${dinnerMenuItems["3"]}(${items["3"]}) Water(${items["3"]}) `;
+  } else if (items["3"] === 1) {
+    reciept += `${dinnerMenuItems["3"]} Water `;
+  } else {
+    reciept += "Water ";
+  }
+  if (items["4"] > 1) {
+    reciept += `${dinnerMenuItems["4"]}(${items["4"]}) `;
+  } else if (!items["4"]) {
+    return "Unable to process. Dessert is missing";
+  } else {
+    reciept += `${dinnerMenuItems["4"]}`;
+  }
+  console.log(reciept);
+  return reciept;
+}
+
+module.exports = {
+  processOrder,
+  breakfastOrder,
+  lunchOrder,
+  dinnerOrder,
+};
